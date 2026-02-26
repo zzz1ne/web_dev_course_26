63b80f0ba137ffaabb8d40ef46400fdd392abaf8
zzz1ne <125074133+zzz1ne@users.noreply.github.com>
2026-02-26 13:46:49 +0000

    lab3/factory.rb done

--- labs/lab3/factory.rb
+++ labs/lab3/factory.rb
@@ -26,7 +26,7 @@ class Car < Vehicle
   def initialize
     # TODO: Call super with appropriate values
     # Car: 4 wheels, 5 passengers
-    nil
+    super('Car', 4, 5)
   end
 end
 
@@ -34,7 +34,7 @@ class Motorcycle < Vehicle
   def initialize
     # TODO: Call super with appropriate values
     # Motorcycle: 2 wheels, 2 passengers
-    nil
+    super('Motorcycle', 2, 2)
   end
 end
 
@@ -42,7 +42,7 @@ class Truck < Vehicle
   def initialize
     # TODO: Call super with appropriate values
     # Truck: 6 wheels, 3 passengers
-    nil
+    super('Truck', 6, 3)
   end
 end
 
@@ -53,7 +53,16 @@ class VehicleFactory
   # Return nil for unknown types
   
   def self.create_vehicle(type)
-    nil
+    case type
+    when :car
+      Car.new
+    when :motorcycle
+      Motorcycle.new
+    when :truck
+      Truck.new
+    else
+      nil
+    end
   end
 end
 
@@ -70,7 +79,7 @@ class EmailNotification < Notification
   # TODO: Implement send method
   # Return "Email sent: #{message}"
   def send(message)
-    nil
+    "Email sent: #{message}"
   end
 end
 
@@ -78,7 +87,7 @@ class SMSNotification < Notification
   # TODO: Implement send method
   # Return "SMS sent: #{message}"
   def send(message)
-    nil
+    "SMS sent: #{message}"
   end
 end
 
@@ -86,7 +95,7 @@ class PushNotification < Notification
   # TODO: Implement send method
   # Return "Push notification sent: #{message}"
   def send(message)
-    nil
+    "Push notification sent: #{message}"
   end
 end
 
@@ -95,9 +104,18 @@ class NotificationFactory
   # Input: type (:email, :sms, :push)
   # Output: instance of appropriate notification class
   
-  def self.create(type)
-    nil
-  end
+   def self.create(type)
+    case type
+    when :email
+      EmailNotification.new
+    when :sms
+      SMSNotification.new
+    when :push
+      PushNotification.new
+    else
+      nil
+    end
+   end
 end
 
 # Exercise 3: Abstract Factory Pattern
@@ -115,7 +133,7 @@ class LightButton
   # TODO: Implement render method
   # Return "Rendering light button"
   def render
-    nil
+    "Rendering light button"
   end
 end
 
@@ -125,7 +143,7 @@ class DarkButton
   # TODO: Implement render method
   # Return "Rendering dark button"
   def render
-    nil
+    "Rendering dark button"
   end
 end
 
@@ -135,7 +153,7 @@ class LightTextField
   # TODO: Implement render method
   # Return "Rendering light text field"
   def render
-    nil
+    "Rendering light text field"
   end
 end
 
@@ -145,7 +163,7 @@ class DarkTextField
   # TODO: Implement render method
   # Return "Rendering dark text field"
   def render
-    nil
+    "Rendering dark text field"
   end
 end
 
@@ -164,11 +182,11 @@ class LightThemeFactory < ThemeFactory
   # TODO: Implement create_text_field to return LightTextField
   
   def create_button
-    nil
+    LightButton.new
   end
   
   def create_text_field
-    nil
+    LightTextField.new
   end
 end
 
@@ -177,11 +195,11 @@ class DarkThemeFactory < ThemeFactory
   # TODO: Implement create_text_field to return DarkTextField
   
   def create_button
-    nil
+    DarkButton.new
   end
   
   def create_text_field
-    nil
+    DarkTextField.new
   end
 end
 
